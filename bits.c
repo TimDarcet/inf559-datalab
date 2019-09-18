0325ed96abf638020478d317a15ad31be7a8f72a
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-09-18 11:27:55 +0200

    test

--- bits.c
+++ bits.c
@@ -147,7 +147,7 @@ int func2(int x, int y) {
  */
 int func3(int n) {
   printf("%d ; %d ; %d ; %d ; %d\n", n, ~0, (~n + 1), (32 + (~n + 1)), 0);
-  return (~0 << 32);
+  return (~0 << 31);
 }
 
 /* 
